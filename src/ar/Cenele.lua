d0cb9dd22a0c7dc33e727993f9cfc7ee8d9a0e19
Ahmed2556789 <hamaddoomi5@gmail.com>
2026-09-10 12:48:05 +0100

    Update Cenele ID from 4510 to 4501

--- src/ar/Cenele.lua
+++ src/ar/Cenele.lua
@@ -1,7 +1,7 @@
--- {"id":4510,"ver":"1.0.0","libVer":"1.0.0","author":"Ahmed2556789","repo":"https://github.com/Ahmed2556789/Shosetsu-Arabic","dep":["Madara>=2.3.2"]}
+-- {"id":4501,"ver":"1.0.0","libVer":"1.0.0","author":"Ahmed2556789","repo":"https://github.com/Ahmed2556789/Shosetsu-Arabic","dep":["Madara>=2.3.2"]}
 
 return Require("Madara")("https://cenele.com", {
-    id = 4510,
+    id = 4501,
     name = "Cenele",
     imageURL = "https://cenele.com/wp-content/uploads/2023/01/cropped-logo.png"
 })
