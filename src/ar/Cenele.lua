f1c488a73b9e850070ec607e6873883819ba949f
Ahmed2556789 <hamaddoomi5@gmail.com>
2026-09-10 12:42:34 +0100

    Test Cenele with Madara

--- src/ar/Cenele.lua
+++ src/ar/Cenele.lua
@@ -1,8 +1,7 @@
--- {"id":4510,"ver":"1.0.0","libVer":"1.0.0","author":"Ahmed2556789","repo":"https://github.com/Ahmed2556789/Shosetsu-Arabic","dep":[]}
+-- {"id":4510,"ver":"1.0.0","libVer":"1.0.0","author":"Ahmed2556789","repo":"https://github.com/Ahmed2556789/Shosetsu-Arabic","dep":["Madara>=2.3.2"]}
 
-return {
+return Require("Madara")("https://cenele.com", {
     id = 4510,
     name = "Cenele",
-    imageURL = "https://cenele.com/wp-content/uploads/2023/01/cropped-logo.png",
-    lang = "ar"
-}
+    imageURL = "https://cenele.com/wp-content/uploads/2023/01/cropped-logo.png"
+})
